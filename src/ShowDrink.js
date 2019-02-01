f98c7bda3f6f1d8159897eac4ecc2cf17828f4b6
009kings <009kings@gmail.com>
2019-02-01 11:58:11 -0800

    sum display changes

--- src/ShowDrink.js
+++ src/ShowDrink.js
@@ -13,9 +13,18 @@ class ShowDrink extends Component{
 	}		
 
 	render(){
+    const img = this.state.drink.image ? <image src={this.state.drink.image} /> : <p></p>
+
 		return(
 			<div className="show-drink">
-				<h2>{this.state.drink.name}</h2>
+				<h2 className="ttu">{this.state.drink.name}</h2>
+        <p><em>by {this.state.drink.brand}</em></p>
+        {img}
+        <p><strong>Price:</strong> ${this.state.drink.price}</p>
+        <p><strong>Type:</strong> {this.state.drink.type}</p>
+        <p><strong>Subtype:</strong> {this.state.drink.subtype}</p>
+        <p><strong>Description:</strong> {this.state.drink.desc}</p>
+
 				<EditDrinkForm drink={this.props.drink} />
 			</div>
 		)
